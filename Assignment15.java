48310c9713cf724d81d2a9ae9f5c800907007c5e
Mohammed Nurul Abedin Ashraf <nurulabedinashraf@gmail.com>
2022-09-26 21:57:42 -0500

    added Assignment15.java

---
+++ Assignment15.java
@@ -0,0 +1,13 @@
+//Mohammed Nurul Abedin Ashraf
+//CS-155(The Fundamental of Computer Programming)
+//Dr. Art Kazmierczak (Dr. K)
+//Prog 1
+//Assignment 1
+//Exercise 1.5
+//08-28-2022
+
+public class Assignment15 {
+    public static void main(String[] args) {
+        System.out.println(((9.5*4.5)-(2.5*3))/(45.5-3.5));
+    }
+}
