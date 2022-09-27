3f32a082ec75a6ca48fd0bfeb2153057f1a61e98
Mohammed Nurul Abedin Ashraf <nurulabedinashraf@gmail.com>
2022-09-26 22:01:09 -0500

    added Assignment11.java

---
+++ Assignment11.java
@@ -0,0 +1,13 @@
+//Mohammed Nurul Abedin Ashraf
+//CS-155(The Fundamental of Computer Programming)
+//Dr. Art Kazmierczak (Dr. K)
+//08/28/2022
+
+
+public class Assignment11 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java.");
+        System.out.println("Welcome to Computer Science.");
+        System.out.println("Programming is fun.");
+    }
+}
