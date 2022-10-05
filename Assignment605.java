3639bd29477b55c4c34a451912629b9dc31f370a
Mohammed Nurul Abedin Ashraf <nurulabedinashraf@gmail.com>
2022-10-05 16:13:05 -0500

    modified Assignment605.java

--- Assignment605.java
+++ Assignment605.java
@@ -1,3 +1,13 @@
+/*
+ (Sort three numbers) Write a method with the following header to display three 
+numbers in increasing order:
+public static void displaySortedNumbers(
+ double num1, double num2, double num3)
+Write a test program that prompts the user to enter three numbers and invokes the 
+method to display them in increasing order.
+ */
+
+
 //Mohammed Nurul Abedin Ashraf
 //CS-155(The Fundamental of Computer Programming)
 //Dr. Art Kazmierczak (Dr. K)
