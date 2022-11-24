50081c1d1f4d0b007708197b6cc187a2a0135f29
Mohammed Nurul Abedin Ashraf <nurulabedinashraf@gmail.com>
2022-11-24 16:37:05 -0600

    Modified Assignment13_01.java

--- Assignment13_01.java
+++ Assignment13_01.java
@@ -1,3 +1,14 @@
+/*********************************************************************************
+* (Triangle class) Design a new Triangle class that extends the abstract         *
+* GeometricObject class. Draw the UML diagram for the classes Triangle and       *
+* GeometricObject and then implement the Triangle class. Write a test program    *
+* that prompts the user to enter three sides of the triangle, a color, and a     *
+* Boolean value to indicate whether the triangle is filled. The program should   *
+* create a Triangle object with these sides and set the color and filled         *
+* properties using the input. The program should display the area, perimeter,    *
+* color, and true or false to indicate whether it is filled or not.              *
+*********************************************************************************/
+
 import java.util.Scanner;
 
 public class Assignment13_01 {
